3ef802cdfb16747a707cf610d3f47ebb84a095a7
zhongshuai <zhongshuai@megvii.com>
2019-06-03 16:04:30 +0800

    m

---
+++ restart.sh
@@ -0,0 +1,10 @@
+###
+ # @Description: 
+ # @Author: zhongshuai
+ # @Date: 2019-06-03 16:03:00
+ # @LastEditors: zhongshuai
+ # @LastEditTime: 2019-06-03 16:04:27
+###
+ git pull
+ npm run build
+ pm2 restart auth-platform-web
