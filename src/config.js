ab9ad1f98cd168b230a3c6a9838bd4af8f70ca4e
zhongshuai <zhongshuai@megvii.com>
2019-06-12 10:10:39 +0800

    切换ip

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@
  * @Author: zhongshuai
  * @Date: 2019-06-09 18:11:43
  * @LastEditors: zhongshuai
- * @LastEditTime: 2019-06-09 22:04:44
+ * @LastEditTime: 2019-06-12 10:10:02
  */
-const url = 'http://127.0.0.1:1114/';
+const url = 'http://10.231.20.63:1114/';
 export default {
   baseUiUrl: url + 'upload/unzip/',
   baseURL: url + 'v1/service/'
