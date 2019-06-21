e26f89df9486dabe72b85c8ac25e35ace952dc4f
zhongshuai <zhongshuai@megvii.com>
2019-06-21 17:27:15 +0800

    m

--- src/config.js
+++ src/config.js
@@ -3,9 +3,10 @@
  * @Author: zhongshuai
  * @Date: 2019-06-09 18:11:43
  * @LastEditors: zhongshuai
- * @LastEditTime: 2019-06-12 10:10:02
+ * @LastEditTime: 2019-06-21 17:26:07
  */
-const url = 'http://10.231.20.63:1114/';
+const ip = '10.231.20.63' || process.env.design_ip;
+const url = `http://${ip}:1114/`;
 export default {
   baseUiUrl: url + 'upload/unzip/',
   baseURL: url + 'v1/service/'
