2f3ec636ad3a0ec4a6a83e7615ad066f3614c3a8
zhongshuai <zhongshuai@megvii.com>
2019-07-02 18:58:11 +0800

    docker

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@
  * @Author: zhongshuai
  * @Date: 2019-06-09 18:11:43
  * @LastEditors: zhongshuai
- * @LastEditTime: 2019-07-01 16:15:42
+ * @LastEditTime: 2019-07-02 18:58:02
  */
-const ip = 'localhost' || process.env.design_ip;
+const ip = '10.231.20.63' || process.env.design_ip;
 const url = `http://${ip}:1114/`;
 export default {
   baseUiUrl: url + 'upload/unzip/',
