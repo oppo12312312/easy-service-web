9c4750f0d45bb6fdb6722ccef0a7cabe87936693
zhongshuai <zhongshuai@megvii.com>
2019-07-03 18:34:28 +0800

    合并

--- src/page/layout/title.vue
+++ src/page/layout/title.vue
@@ -3,13 +3,13 @@
  * @Author: zhongshuai
  * @Date: 2019-06-02 15:00:53
  * @LastEditors: zhongshuai
- * @LastEditTime: 2019-07-03 18:29:26
+ * @LastEditTime: 2019-07-03 18:32:22
  -->
 <template>
   <div class="title">
     <div class="logo"></div>
     <div class="text">
-      Fix Design
+      Flix Design
     </div>
   </div>
 </template>
