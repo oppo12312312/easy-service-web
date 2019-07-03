3e3d564b6b761fe27a92f906ea6973afec81ebb6
zhongshuai <zhongshuai@megvii.com>
2019-07-03 18:29:40 +0800

    title

--- src/page/layout/title.vue
+++ src/page/layout/title.vue
@@ -3,13 +3,13 @@
  * @Author: zhongshuai
  * @Date: 2019-06-02 15:00:53
  * @LastEditors: zhongshuai
- * @LastEditTime: 2019-06-09 18:31:39
+ * @LastEditTime: 2019-07-03 18:29:26
  -->
 <template>
   <div class="title">
     <div class="logo"></div>
     <div class="text">
-      Design
+      Fix Design
     </div>
   </div>
 </template>
